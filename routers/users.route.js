bd1f701d95a03aabcbd2d4720f6bd49f438521dc
mariananbp <mariananbp@gmail.com>
2022-02-04 18:29:16 -0300

    router - find by id

--- routers/users.route.js
+++ routers/users.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { createUser, getAllUsers } = require('../controllers/user.controller');
+const { createUser, getAllUsers, getUser } = require('../controllers/user.controller');
 const auth = require('../middlewares/auth');
 
 const router = express.Router();
 
 router.post('/', createUser);
 router.get('/', auth, getAllUsers);
+router.get('/:id', auth, getUser);
 
 module.exports = router;
