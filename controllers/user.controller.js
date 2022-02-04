da74881e8a82b842b694b5a6ee33a0ab5a11675d
mariananbp <mariananbp@gmail.com>
2022-02-04 15:24:55 -0300

    controller - create user

---
+++ controllers/user.controller.js
@@ -0,0 +1,17 @@
+const { userCreate } = require('../services/user.service');
+const { created } = require('../utils/dictionary/statusCode');
+
+const createUser = async (req, res, next) => {
+  try {
+    const user = req.body;
+    const token = await userCreate(user);
+
+    return res.status(created).json({ token });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  createUser,
+};
