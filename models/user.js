593b9e1643bd8360e41e46cc71c962dfca6d22b9
mariananbp <mariananbp@gmail.com>
2022-02-04 15:24:07 -0300

    models - remove validation by sequelize

--- models/user.js
+++ models/user.js
@@ -1,60 +1,10 @@
-/* const validateName = {
-    notEmpty: true,
-    min: {
-      args: 8,
-    msg: '"displayName" length must be at least 8 characters long',
-    },
-};
-
-const validateEmail = {
-  isEmail: true,
-  notEmpty: true,
-  unique: {
-    args: true,
-    msg: 'User already registered',
-  },
-};
-
-const validatePassword = {
-  notEmpty: true,
-  len: 6,
-}; */
-
-function User(sequelize, DataTypes) {
-  const User = sequelize.define('User', {
-    displayName: { type: DataTypes.STRING,
-      validate: {
-        notEmpty: true,
-        min: {
-          args: 8,
-        msg: '"displayName" length must be at least 8 characters long',
-        },
-    },
-    },
-    email: {
-      type: DataTypes.STRING,
-      validate: {
-        isEmail: true,
-        notEmpty: true,
-        unique: {
-          args: true,
-          msg: 'User already registered',
-        },
-      },
-    },
-    password: {
-      type: DataTypes.STRING,
-      validate: {
-        notEmpty: true,
-        len: 6,
-      },
-    },
+module.exports = (sequelize, DataTypes) => 
+  sequelize.define('User', {
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
     image: DataTypes.STRING,
   },
   { underscored: true,
        tableName: 'Users',
      });
-  return User;
-}
-
-module.exports = User;
