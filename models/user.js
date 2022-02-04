5f41687d740c0342297e26a2011fd6a1b359a443
mariananbp <mariananbp@gmail.com>
2022-02-04 18:18:26 -0300

    timestamps false

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ module.exports = (sequelize, DataTypes) =>
     password: DataTypes.STRING,
     image: DataTypes.STRING,
   },
-  { underscored: true,
-       tableName: 'Users',
-     });
+  { 
+    tableName: 'Users',
+    timestamps: false,
+  });
